7cc85c7b6edbf062cc223d1b9fe23c7b1a5a73a8
Dustin Norlander <dustinn@gmail.com>
2013-01-09 16:57:14 -0800

    start migrating to new cheshire

--- wildling.go
+++ wildling.go
@@ -1,9 +1,44 @@
 package main
+import (
+	"log"
+	"github.com/trendrr/cheshire-golang/cheshire"
+	"flag"
+	"os"
+	"fmt"
+)
 
+func init(){
+	flag.StringVar(&config, "config", "relative", "path to the app config.yaml")
+}
+
+var config string 
+
+func main() {
+
+	//parse the command line args
+	flag.Parse()
+
+	//make sure the linker includes our controllers and runs inits
+	// controllers.Load()
+
+	//if the config isn't specified on the command line then try the default relative location
+	if config == "relative" {
+		//get our current working dir
+		app_path, _ := os.Getwd()
+		//create our config path based on assumption
+		config = fmt.Sprintf("%s/config/config.yaml", app_path)
+	}
+	bootstrap := cheshire.NewBootstrapFile(config)
+	log.Println(bootstrap)
+	//tell everyone we started up
+	log.Println("starting app with config="+config)
+}
+
+/*
 import (
 	"fmt"
-	"github.com/mdennebaum/cheshire"
-	"kipple/app/controllers"
+	"github.com/trendrr/cheshire-golang"
+	"app/controllers"
 	"log"
 	"os"
 	"flag"
@@ -36,3 +71,4 @@ func main() {
 	//start a new server
 	cheshire.NewBootstrap(config).Cheshire.Start()
 }
+*/
